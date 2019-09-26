055e5593dba1b3d697de34fc77768f1726905718
Webs <weberswords@gmail.com>
2019-09-26 11:32:18 -0700

    First commit

---
+++ Lesson.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+
+class Lesson extends Component {
+    constructor(props) {
+        super(props);
+     }
+
+    render() {
+        return (
+            <Fragment>
+             
+            </Fragment>
+        );
+    }
+}
+
+export default Lesson;
